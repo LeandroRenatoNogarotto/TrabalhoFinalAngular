cf370f04943da40fbe835a0337714501e11ff4a7
franciscoldb <fluis905@gmail.com>
2023-02-14 20:34:26 -0300

    First commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { PaginaLoginComponent } from './pagina-login/pagina-login.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    PaginaLoginComponent
   ],
   imports: [
     BrowserModule,
