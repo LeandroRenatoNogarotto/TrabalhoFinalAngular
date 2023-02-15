b9a4ce1b992af9f32841841a1828fe232ac999ac
franciscoldb <fluis905@gmail.com>
2023-02-14 21:40:06 -0300

    adicionando elementos da tela

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { FormCadastroComponent } from './form-cadastro/form-cadastro.component';
+import { FormLoginComponent } from './form-login/form-login.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: 'Login', component: FormLoginComponent},
+  {path: 'Cadastro', component: FormCadastroComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,15 +4,21 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { PaginaLoginComponent } from './pagina-login/pagina-login.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { FormLoginComponent } from './form-login/form-login.component';
+import { FormCadastroComponent } from './form-cadastro/form-cadastro.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    PaginaLoginComponent
+    PaginaLoginComponent,
+    FormLoginComponent,
+    FormCadastroComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
