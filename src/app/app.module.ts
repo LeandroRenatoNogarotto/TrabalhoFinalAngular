aee079b0e84d135c89b551f7932653bc26529bc7
Leandro.Renato.Nogarotto <leandronogarotto@alunos.utfpr.edu.br>
2023-02-14 21:42:59 -0300

    changes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,25 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { PaginaPrincipalComponent } from './pagina-principal/pagina-principal.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { CardMetaComponent } from './card-meta/card-meta.component';
+import {MatProgressBarModule} from '@angular/material/progress-bar';
+import {MatCardModule} from '@angular/material/card';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    PaginaPrincipalComponent,
+    CardMetaComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    MatProgressBarModule,
+    MatCardModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/card-meta/card-meta.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-card-meta',
+  templateUrl: './card-meta.component.html',
+  styleUrls: ['./card-meta.component.css']
+})
+export class CardMetaComponent {
+    value = 25;
+    bufferValue = 100;
+}
